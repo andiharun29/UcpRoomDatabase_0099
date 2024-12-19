d9f58854fe1499cdb4b3e64e32954357aeb51ea8
andiharun29 <andiharunalrasyid14@gmail.com>
2024-12-20 03:41:03 +0700

    Tambahkan SupplierDao dengan operasi insert dan pengambilan data

---
+++ app/src/main/java/com/example/ucp2/Data/Database/InventarisDatabase.kt
@@ -0,0 +1,31 @@
+package com.example.ucp2.Data.Database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.ucp2.Data.Entity.Barang
+import com.example.ucp2.Data.Entity.Supplier
+import com.example.ucp2.Data.dao.BarangDao
+import com.example.ucp2.Data.dao.SupplierDao
+
+@Database(entities = [Barang::class, Supplier::class], version = 1, exportSchema = false)
+abstract class InventarisDatabase : RoomDatabase(){
+    abstract fun barangDao(): BarangDao
+    abstract fun supplierDao() : SupplierDao
+    companion object{
+        @Volatile
+        private var Instance: InventarisDatabase? = null
+
+        fun getDatabase(context: Context): InventarisDatabase{
+            return (Instance?: synchronized(this){
+                Room.databaseBuilder(
+                    context,
+                    InventarisDatabase::class.java,
+                    "InventarisDatabase"
+                )
+                    .build().also { Instance = it }
+            })
+        }
+    }
+}
