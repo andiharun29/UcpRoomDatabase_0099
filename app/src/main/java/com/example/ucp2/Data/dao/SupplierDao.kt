9c2130499fd164d775bde2593d108f6dc60a3cab
andiharun29 <andiharunalrasyid14@gmail.com>
2024-12-20 03:35:31 +0700

    Tambahkan SupplierDao dengan operasi insert dan pengambilan data

---
+++ app/src/main/java/com/example/ucp2/Data/dao/SupplierDao.kt
@@ -0,0 +1,20 @@
+package com.example.ucp2.Data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.ucp2.Data.Entity.Supplier
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SupplierDao {
+    @Insert
+    suspend fun insertSupplier(
+        supplier: Supplier
+    )
+    @Query("SELECT * FROM Supplier ORDER BY nama ASC")
+    fun getAllSupplier(): Flow<List<Supplier>>
+
+    @Query("SELECT * FROM Supplier WHERE id = :id")
+    fun getSupplier(id: String): Flow<Supplier>
+}
