5a625fe2189017776937863955ed563f913b92a2
andiharun29 <andiharunalrasyid14@gmail.com>
2024-12-24 01:55:04 +0700

    Menambahkan fungsi untuk mengonversi entitas Barang menjadi BarangEvent untuk status UI

---
+++ app/src/main/java/com/example/ucp2/ui/ViewModel/Barang/DetailBarangViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.ucp2.ui.ViewModel.Barang
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.Data.Entity.Barang
+import com.example.ucp2.Repository.Barang.RepositoryBarang
+import com.example.ucp2.ui.Navigation.DestinasiDetailBarang
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+
+
+fun Barang.toDetailUiEvent(): BarangEvent{
+    return BarangEvent(
+        id = id.toString(),
+        nama = nama,
+        deskripsi = deskripsi,
+        harga = harga.toString(),
+        stok = stok.toString(),
+        nama_supplier = nama_supplier
+    )
+}
