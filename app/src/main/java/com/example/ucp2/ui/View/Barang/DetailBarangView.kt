7f9c1629d7188491fcd3fba3b890f318329fd712
andiharun29 <andiharunalrasyid14@gmail.com>
2024-12-24 02:53:50 +0700

    Tambah composable ComponentDetailBarang untuk menampilkan judul dan isi

--- app/src/main/java/com/example/ucp2/ui/View/Barang/DetailBarangView.kt
+++ app/src/main/java/com/example/ucp2/ui/View/Barang/DetailBarangView.kt
@@ -44,6 +44,31 @@ import com.example.ucp2.ui.ViewModel.PenyediaViewModel
 
 
 @Composable
+fun ComponentDetailBarang(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String
+
+){
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
 private fun DeleteConfirmationDialog(
     onDeleteConfirm: () -> Unit,
     onDeleteCencel: () -> Unit,
