4f1645568317f791f77b7eff11e66d0ddb1861d9
andiharun29 <andiharunalrasyid14@gmail.com>
2024-12-24 00:21:44 +0700

    Buat data class SupplierEvent untuk mewakili event supplier

---
+++ app/src/main/java/com/example/ucp2/ui/ViewModel/Supplier/SupplierViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.ucp2.ui.ViewModel.Supplier
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.Data.Entity.Supplier
+import com.example.ucp2.Repository.Supplier.RepositorySupplier
+import kotlinx.coroutines.launch
+
+
+
+data class SupplierEvent(
+    val id: Int = 0,
+    val nama: String = "",
+    val kontak: String = "",
+    val alamat: String = ""
+)
